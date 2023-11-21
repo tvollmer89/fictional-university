7a23d58ab9e9669703427b7cddce930a606b0978
Theresa iMac <tvollmer@carboline.com>
2023-11-21 00:45:54 -0700

    added package.json

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import HeroSlider from "./modules/HeroSlider"
 
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu()
-var heroSlider = new HeroSlider()
+var heroSlider = new HeroSlider();
